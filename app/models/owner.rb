acfe5f3be8352867d6368f9ece0f1ae1ca460c5b
king0120 <king0120@gmail.com>
2017-06-22 17:08:38 -0400

    solution

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,15 @@
 class Owner < ApplicationRecord
-     # TODO: add validations
-
   before_save :normalize_phone_number
 
-  # removes leading 1 and the characters (, ), -, .
-  def normalize_phone_number
-    # stretch
-  end
+  validates :first_name, :last_name, :email, presence: true, length: { maximum: 255 }
 
+  validates :email, uniqueness: true,
+                    inclusion: { in: %w[@] }
+  # format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+
+  def normalize_phone_number
+    return false unless phone.present?
+    phone.gsub!(/^1/, '')
+    phone.gsub!(/[()-.]/, '')
+  end
 end
